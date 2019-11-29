2a6c487d2ff4d7d7279508faf833c617a9b6474c
unknown <papaaced@gmail.com>
2019-11-29 14:56:17 -0500

    Median finder problem

---
+++ DataStructures/295.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.DataStructures
+{
+
+    public class MedianFinder
+    {
+        Heap minHeap = new Heap(Comparer<int>.Create((x, y) => x - y));
+        Heap maxHeap = new Heap(Comparer<int>.Create((x, y) => y-x));
+
+        /** initialize your data structure here. */
+        public MedianFinder()
+        {
+        }
+
+        public void AddNum(int num)
+        {
+            if (minHeap.Count > 0 && num > minHeap.Top)
+            {
+                minHeap.Insert(num);
+                if (maxHeap.Count < minHeap.Count)
+                {
+                    maxHeap.Insert(minHeap.Remove());
+                    Console.WriteLine(maxHeap.Top);
+                }
+            }
+            else
+            {
+                maxHeap.Insert(num);
+                if (maxHeap.Count > minHeap.Count + 1)
+                {
+                    minHeap.Insert(maxHeap.Remove());
+                }
+            }
+        }
+
+        public double FindMedian()
+        {
+            var count = minHeap.Count + maxHeap.Count;
+
+            if(count %2 == 0)
+            {
+                return (minHeap.Top + maxHeap.Top) / 2f;
+            }
+            return maxHeap.Top;
+        }
+    }
+}
