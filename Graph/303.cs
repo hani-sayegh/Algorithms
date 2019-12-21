a001bc8a716a76e90b5d3006cff3ba8df504cf4c
unknown <papaaced@gmail.com>
2019-12-21 17:25:46 -0500

    RSQ

---
+++ Graph/303.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Graph
+{
+    class _303
+    {
+        public class NumArray
+        {
+            int[] prefixSum;
+            public NumArray(int[] nums)
+            {
+                prefixSum = new int[nums.Length + 1];
+                for (int i = 0; i != nums.Length; ++i)
+                {
+                    prefixSum[i + 1] = prefixSum[i] + nums[i];
+                }
+            }
+
+            public int SumRange(int i, int j)
+            {
+                return prefixSum[j + 1] - prefixSum[i];
+            }
+        }
+
+        /**
+         * Your NumArray object will be instantiated and called as such:
+         * NumArray obj = new NumArray(nums);
+         * int param_1 = obj.SumRange(i,j);
+         */
+
+    }
+}
